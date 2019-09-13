87708c4d5b7b389a4d6e922d430f256945ab62a7
Boda <bodavivienviktoria@gmail.com>
2019-09-13 11:14:05 +0200

    IWontCheatOnTheExams added.

---
+++ week-01/day4/src/IWontCheatOnTheExams.java
@@ -0,0 +1,9 @@
+public class IWontCheatOnTheExams {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 100) {
+            System.out.println("I won't cheat on the exam!");
+            a ++;
+        }
+    }
+}
