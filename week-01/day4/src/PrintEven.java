927b2d429209015c23a46f501d1bd5e039c7ebb5
Boda <bodavivienviktoria@gmail.com>
2019-09-13 11:16:12 +0200

    PrintEven added.

---
+++ week-01/day4/src/PrintEven.java
@@ -0,0 +1,9 @@
+public class PrintEven {
+    public static void main(String[] args) {
+        int a = 1;
+        while (a < 500) {
+            System.out.println(a);
+            a +=2;
+        }
+    }
+}
