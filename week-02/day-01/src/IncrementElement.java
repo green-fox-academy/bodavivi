51e6778e113db26bb43e0ae745b8d1b447e713ba
Boda <bodavivienviktoria@gmail.com>
2019-09-16 12:35:40 +0200

    IncrementElement is done.

---
+++ week-02/day-01/src/IncrementElement.java
@@ -0,0 +1,8 @@
+public class IncrementElement {
+    public static void main(String[] args) {
+        int [] t = {1, 2, 3, 4, 5};
+        t[2] = t[2] + 10;
+        System.out.println(t[2]);
+
+    }
+}
