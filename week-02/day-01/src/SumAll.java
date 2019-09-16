0b58aa621cb253bb63c0c24af70c3dbe947b917c
Boda <bodavivienviktoria@gmail.com>
2019-09-16 22:32:02 +0200

    SumAll is updated.

--- week-02/day-01/src/SumAll.java
+++ week-02/day-01/src/SumAll.java
@@ -1,10 +1,12 @@
 public class SumAll {
     public static void main(String[] args) {
-        int [] helpme = {3, 4, 5, 6, 7};
+        int [] ai = {3, 4, 5, 6, 7};
         int sum = 0;
-        for (int number : helpme){
-            sum = sum + number;
+        for (int i = 0; i < ai.length; i++) {
+            sum = sum + ai [i];
+
         }
-        System.out.println(sum);
+        System.out.println("The sum of the elements in ai is: "+sum);
+
     }
 }
