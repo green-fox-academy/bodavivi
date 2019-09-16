27d77234f09e8c127110d47e4efe9795bb93b6b1
Boda <bodavivienviktoria@gmail.com>
2019-09-16 16:19:33 +0200

    AppendAFunc is added.

---
+++ week-02/day-01/src/AppendAFunc.java
@@ -0,0 +1,19 @@
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a string variable named `typo` and assign the value `Chinchill` to it
+// - Write a function called `appendAFunc` that gets a string as an input,
+//   appends an 'a' character to its end and returns with a string
+//
+// - Print the result of `appendAFunc(typo)`
+public class AppendAFunc {
+    public static void main(String[] args) {
+        String typo = "Chinchill";
+        String mouse = appendAFunc(typo);
+        System.out.println(mouse);
+    }
+    public static String appendAFunc (String app){
+        String hamster = app + "a";
+        return hamster;
+    }
+}
