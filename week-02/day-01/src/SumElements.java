1c8aac46657810637b8f88e8ddea86f3a5ed3e7c
Boda <bodavivienviktoria@gmail.com>
2019-09-16 12:23:58 +0200

    SumElements is done.

---
+++ week-02/day-01/src/SumElements.java
@@ -0,0 +1,7 @@
+public class SumElements {
+    public static void main(String[] args) {
+        int [] r = {54, 23, 66, 12};
+        int sum = (r[1] + r[2]);
+        System.out.println(sum);
+    }
+}
