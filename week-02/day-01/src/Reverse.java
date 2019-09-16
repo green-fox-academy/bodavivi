090aba3f76b1fe1a44aee12ac93157c135c8695d
Boda <bodavivienviktoria@gmail.com>
2019-09-16 22:53:02 +0200

    Reverse is updated.

--- week-02/day-01/src/Reverse.java
+++ week-02/day-01/src/Reverse.java
@@ -5,10 +5,15 @@
 
 public class Reverse {
     public static void main(String[] args) {
-        int[] aj = {3, 4, 5, 6, 7};
-        for (int i = aj.length-1; i >= 0 ; i--) {
-            System.out.println(aj[i]);
+        int [] aj = {3, 4, 5, 6, 7};
+        int [] ak = new int[aj.length];
+        for (int i = aj.length-1; i>=0; i--){
+            ak [aj.length-(i+1)] = aj [i];
 
+        }
+        for (int j = 0; j < ak.length; j++){
+            aj [j] = ak[j];
+            System.out.print(aj[j]+", ");
         }
         }
     }
