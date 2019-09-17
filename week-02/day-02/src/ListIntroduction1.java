29adf39e798d206d3e94e29a3b513c19ed9e66f2
Boda <bodavivienviktoria@gmail.com>
2019-09-17 14:10:23 +0200

    ListIntroduction1 is added.

---
+++ week-02/day-02/src/ListIntroduction1.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+public class ListIntroduction1 {
+    public static void main(String[] args) {
+
+        //Create an empty list which will contain names (strings)
+        ArrayList<String> names = new ArrayList<>();
+
+
+        //Print out the number of elements in the list
+        System.out.println(names.size());
+
+
+
+        //Add William to the list
+        names.add ("William");
+
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.print("-");
+
+        }
+        System.out.println();
+
+        //Print out whether the list is empty or not
+        if (names.size() == 0){
+            System.out.println("The list is empty");
+        }
+        else{
+            System.out.println("There are some elements in the list.");
+        }
+
+
+        //Add John to the list
+        //Add Amanda to the list
+        names.add("John");
+        names.add("Amanda");
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.print("-");
+
+        }
+        System.out.println();
+
+        //Print out the number of elements in the list
+
+        System.out.println(names.size());
+
+        for (int i = 0; i <10 ; i++) {
+            System.out.print("-");
+
+        }
+        System.out.println();
+
+        //Print out the 3rd element
+
+        System.out.println(names.get(2));
+        for (int i = 0; i <20 ; i++) {
+
+        }
+        for (int i = 0; i <10 ; i++) {
+            System.out.print("-");
+
+        }
+        System.out.println();
+
+        //Iterate through the list and print out each name
+        for (String name : names){
+            System.out.println(name);
+        }
+        for (int i = 0; i <10 ; i++) {
+            System.out.print("-");
+
+        }
+        System.out.println();
+
+        //Iterate through the list and print
+        for (int j = 0; j < names.size() ; j++) {
+            System.out.println((j+1)+". "+names.get(j) );
+
+        }
+
+
+
+
+    }
+}
+
