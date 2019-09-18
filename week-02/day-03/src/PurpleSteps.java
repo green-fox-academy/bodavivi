e115b0636974dee034bc08169052cb66f2098238
Boda <bodavivienviktoria@gmail.com>
2019-09-18 15:01:23 +0200

    PurpleSteaps is added.

---
+++ week-02/day-03/src/PurpleSteps.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PurpleSteps {
+    public static void mainDraw(Graphics graphics) {
+        // Reproduce this:
+        // [https://github.com/green-fox-academy/teaching-materials/blob/master/workshop/drawing/assets/r3.png]
+
+        for (int i = 0; i <= 500; i += 30) {
+            graphics.setColor(new Color(179, 46, 255));
+            graphics.fillRect(i,i, 30,30);
+            graphics.setColor(new Color(89, 34, 138));
+            graphics.drawRect(i,i, 30,30);
+
+
+        }
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 1000;
+    static int HEIGHT = 1000;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
