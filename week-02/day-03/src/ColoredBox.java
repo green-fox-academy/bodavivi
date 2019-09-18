e184f45c9dcaea994ac359eeec91daf2c1aa5f59
Boda <bodavivienviktoria@gmail.com>
2019-09-18 11:13:26 +0200

    LineInTheMiddle and ColoredBox is added.

---
+++ week-02/day-03/src/ColoredBox.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+import java.awt.*;
+
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class ColoredBox {
+    public static void mainDraw(Graphics graphics) {
+        // Draw a box that has different colored lines on each edge.
+        graphics.setColor(Color.GREEN);
+        graphics.drawLine(100, 100, 300, 100);
+
+        graphics.setColor(Color.RED);
+        graphics.drawLine(100, 100, 100, 300);
+
+        graphics.setColor(Color.CYAN);
+        graphics.drawLine(100, 300, 300, 300);
+
+        graphics.setColor(Color.magenta);
+        graphics.drawLine(300, 100, 300, 300);
+
+
+
+
+    }
+
+    // Don't touch the code below
+    static int WIDTH = 1000;
+    static int HEIGHT = 1000;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        ImagePanel panel = new ImagePanel();
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.add(panel);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+        jFrame.pack();
+    }
+
+    static class ImagePanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+        }
+    }
+}
