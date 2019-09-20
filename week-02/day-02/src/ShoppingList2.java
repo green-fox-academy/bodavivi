e5c21cca29bc3458a72202ebdf31e52747ca396c
Boda <bodavivienviktoria@gmail.com>
2019-09-20 13:17:38 +0200

    ShoppingList2 is done.

--- week-02/day-02/src/ShoppingList2.java
+++ week-02/day-02/src/ShoppingList2.java
@@ -1,70 +1,120 @@
 import java.sql.SQLOutput;
 import java.util.HashMap;
 
-public class ShoppingList2 {
+public class ShoppingList2{
     public static void main(String[] args) {
-        HashMap<String, Double> productBase = new HashMap<>();
+        HashMap<String, Double> productsWithPrice = new HashMap<>();
+        productsWithPrice.put("Milk", 1.07);
+        productsWithPrice.put("Rice", 1.59);
+        productsWithPrice.put("Eggs", 3.14);
+        productsWithPrice.put("Cheese", 12.60);
+        productsWithPrice.put("Chicken Breasts", 9.40);
+        productsWithPrice.put("Apples", 2.31);
+        productsWithPrice.put("Tomato", 2.58);
+        productsWithPrice.put("Potato", 1.75);
+        productsWithPrice.put("Onion", 1.10);
 
-        productBase.put("Milk", 1.07);
-        productBase.put("Rice", 1.59);
-        productBase.put("Eggs", 3.14);
-        productBase.put("Cheese", 12.6);
-        productBase.put("Chicken Breast", 9.4);
-        productBase.put("Apples", 2.31);
-        productBase.put("Tomato", 2.58);
-        productBase.put("Potato", 1.75);
-        productBase.put("Onion", 1.1);
+        HashMap<String, Integer> bob= new HashMap<>();
+        bob.put("Milk", 3);
+        bob.put("Rice", 2);
+        bob.put("Eggs",2);
+        bob.put("Cheese", 1);
+        bob.put("Chicken Breasts", 4);
+        bob.put("Apples", 1);
+        bob.put("Tomato", 2);
+        bob.put("Potato",1);
 
-        HashMap<String, Integer> bobs = new HashMap<>();
-
-        bobs.put("Milk", 3);
-        bobs.put("Rice", 2);
-        bobs.put("Eggs", 2);
-        bobs.put("Cheese", 1);
-        bobs.put("Chicken Breast", 4);
-        bobs.put("Apples", 1);
-        bobs.put("Tomato", 2);
-        bobs.put("Potato", 1);
-
-        HashMap<String, Integer> alices = new HashMap<>();
-
-        alices.put("Rice", 1);
-        alices.put("Eggs", 1);
-        alices.put("Chicken Breast", 2);
-        alices.put("Apples", 1);
-        alices.put("Tomato", 10);
+        HashMap<String, Integer> alice = new HashMap<>();
+        alice.put("Rice", 1);
+        alice.put("Eggs", 5);
+        alice.put("Chicken Breasts", 2);
+        alice.put("Apples", 1);
+        alice.put("Tomato", 10);
 
         //How much does Bob pay?
 
-        double BHasToPay = 0;
+        double sumA = 0;
 
-        for (String prods : bobs.keySet()){
-            BHasToPay = (BHasToPay) + (bobs.get(prods)* productBase.get(prods));
+        for (String product : bob.keySet()){
+            sumA = sumA + (productsWithPrice.get(product) * bob.get(product));
         }
 
-        System.out.println("Bob has to pay " + BHasToPay);
+        System.out.println("The summa of Bobs shopping is " +sumA);
 
         //How much does Alice pay?
-        double AHasToPay = 0;
+        double sumB = 0;
 
-        for (String product : alices.keySet()) {
-            AHasToPay += alices.get(product) * productBase.get(product);
+        for (String product : alice.keySet()){
+            sumB = sumB + (productsWithPrice.get(product) * alice.get(product));
         }
 
-        System.out.println("Alice has to pay " + AHasToPay);
+        System.out.println("The summa of Alices shopping is " +sumB);
 
         //Who buys more Rice?
 
-        if (bobs.get("Rice") > alices.get("Rice")){
-            System.out.println("Bob gets more rice");
+        if (bob.get("Rice") > alice.get("Rice")){
+            System.out.println("Bob buys more rice than Alice.");
         }
-        else if (bobs.get("Rice") < alices.get("Rices")){
-            System.out.println("Alice gets more rice");
+        else if (bob.get("Rice") == alice.get("Rice")){
+            System.out.println("Alice and Bob buy the same amount of rice.");
+
+        }
+        else{
+            System.out.println("Alice buys more rice than Bob.");
         }
 
         //Who buys more Potato?
-
+        if (alice.containsKey("Potato")){
 
         }
+        else{
+            alice.put("Potato", 0);
+        }
 
+        if (bob.get("Potato") > alice.get("Potato")){
+            System.out.println("Bob buys more Potatoes");
+        }
+        else if (bob.get("Potato") == alice.get("Potatoes")){
+            System.out.println("Bob and Alice buy the same amount of potatoes");
+        }
+        else{
+            System.out.println("Alice buys more potatoes.");
+        }
+
+
+        //Who buys more different products?
+        if (bob.size()> alice.size()){
+            System.out.println("Bob buys more products.");
+        }
+        else if (bob.size() == alice.size()){
+            System.out.println("Bob and Alice buy the same amount of products.");
+        }
+        else{
+            System.out.println("Alice buys more products.");
+        }
+
+
+        //Who buys more products? (piece)
+        int bobSum = 0;
+        for (int piece : bob.values()) {
+            bobSum += piece;
+        }
+        int aliceSum = 0;
+        for (int piece : alice.values()){
+            aliceSum += piece;
+        }
+
+        if (bobSum > aliceSum){
+            System.out.println("Bob buys more products");
+        }
+        else if (bobSum == aliceSum){
+            System.out.println("Bob and Alice buy the same amount of products.");
+        }
+        else{
+            System.out.println("Alice buys more products.");
+        }
+
+
+
+    }
 }
