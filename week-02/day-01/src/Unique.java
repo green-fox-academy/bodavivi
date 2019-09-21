07d0f4e0a5f8ba7f1e22866e861aead1ba37de27
Boda <bodavivienviktoria@gmail.com>
2019-09-21 19:20:51 +0200

    Unique is reformated.

--- week-02/day-01/src/Unique.java
+++ week-02/day-01/src/Unique.java
@@ -3,55 +3,54 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Unique {
-    public static void main(String[] args) {
-        System.out.println("How many numbers do you wanna add?");
-        Scanner scanner = new Scanner(System.in);
-        int numberOfArray = scanner.nextInt();
+  public static void main(String[] args) {
+    System.out.println("How many numbers do you wanna add?");
+    Scanner scanner = new Scanner(System.in);
+    int numberOfArray = scanner.nextInt();
 
-        int[] unique = new int[numberOfArray];
+    int[] unique = new int[numberOfArray];
 
-        System.out.println("What r the numbers?");
-        int number = 0;
-        for (int i = 0; i < numberOfArray; i++) {
-            Scanner scanner2 = new Scanner(System.in);
-            number = scanner2.nextInt();
-            unique[i] = number;
-        }
-
-        array(unique, numberOfArray);
-
-
-
+    System.out.println("What r the numbers?");
+    int number = 0;
+    for (int i = 0; i < numberOfArray; i++) {
+      Scanner scanner2 = new Scanner(System.in);
+      number = scanner2.nextInt();
+      unique[i] = number;
     }
 
-    public static void array(int [] unique, int numberOfArray) {
+    array(unique, numberOfArray);
 
 
-        int isItExist = 0;
-        ArrayList<Integer> realArrayList = new ArrayList<>();
+  }
+
+  public static void array(int[] unique, int numberOfArray) {
 
 
-        for (int i = 0; i < numberOfArray; i++) {
-            for (int j = realArrayList.size() - 1; j >= 0; j--) {
-                if (unique[i] == realArrayList.get(j)) {
-                    isItExist = 1;
-                }
-            }
-            if (isItExist == 0) {
-                realArrayList.add(unique[i]);
-            }
-            isItExist = 0;
+    int isItExist = 0;
+    ArrayList<Integer> realArrayList = new ArrayList<>();
+
+
+    for (int i = 0; i < numberOfArray; i++) {
+      for (int j = realArrayList.size() - 1; j >= 0; j--) {
+        if (unique[i] == realArrayList.get(j)) {
+          isItExist = 1;
         }
-
-
-        int[] result = new int[realArrayList.size()];
-        System.out.print("The array contains the numbers of: ");
-        for (int i = 0; i < result.length; i++) {
-            result[i] = realArrayList.get(i);
-            System.out.printf(result[i] +", ");
-        }
-
+      }
+      if (isItExist == 0) {
+        realArrayList.add(unique[i]);
+      }
+      isItExist = 0;
     }
+
+
+    int[] result = new int[realArrayList.size()];
+    System.out.print("The array contains the numbers of: ");
+    for (int i = 0; i < result.length; i++) {
+      result[i] = realArrayList.get(i);
+      System.out.printf(result[i] + ", ");
+    }
+
+  }
 
 
 }
