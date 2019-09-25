faaf8ed77fc60269cfa9dee7d219db0e1195edc4
Boda <bodavivienviktoria@gmail.com>
2019-09-25 12:45:49 +0200

    Animal is updated.

--- week-03/Day-03/src/animal/Animal.java
+++ week-03/Day-03/src/animal/Animal.java
@@ -10,16 +10,16 @@ public class Animal {
   }*/
 
   public void eat(){
-    hunger+= 1;
+    hunger-= 1;
   }
 
   public void drink(){
-    thirst+= 1;
+    thirst-= 1;
   }
 
   public void play(){
-    thirst+= 1;
-    hunger+= 1;
+    thirst-= 1;
+    hunger-= 1;
   }
 
 }
