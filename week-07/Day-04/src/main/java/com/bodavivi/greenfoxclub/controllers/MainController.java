28d3e8a6b2e24f8beec8b0ea4c5d212a140f00df
Boda <bodavivienviktoria@gmail.com>
2019-10-31 13:45:20 +0100

    greenfoxclub index and create are added.

--- week-07/Day-04/src/main/java/com/bodavivi/greenfoxclub/controllers/MainController.java
+++ week-07/Day-04/src/main/java/com/bodavivi/greenfoxclub/controllers/MainController.java
@@ -1,20 +1,37 @@
 package com.bodavivi.greenfoxclub.controllers;
 
+import com.bodavivi.greenfoxclub.models.Fox;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class MainController {
+  List<Fox> foxes = new ArrayList<>();
 
   @GetMapping(value = {"", "/"})
   public String index(Model model, @RequestParam (name = "name", required = false) String name) {
+    Fox actualfox = new Fox("anonymus", "kavics", "viz");
+
+    for(Fox fox : foxes){
+      if (fox.getName().equals(name)){
+       actualfox = fox;
+      }
+    }
+
     if (name == null){
       return "login";
     }else {
-      model.addAttribute("name", name);
+      model.addAttribute("name", actualfox.getName());
+      model.addAttribute("food", actualfox.getFood());
+      model.addAttribute("drink", actualfox.getDrink());
+      model.addAttribute("tricks", actualfox.getTricks().size());
       return "index";
     }
   }
@@ -23,8 +40,20 @@ public class MainController {
   public String logIn(){
     return "login";
   }
+
   @PostMapping(value = {"/login"})
   public String logIn(@RequestParam String name) {
     return "redirect:/?name=" + name;
+  }
+
+  @GetMapping(value ={"/create"})
+  public String createForm(@ModelAttribute("fox") Fox fox) {
+    return "create";
+  }
+
+  @PostMapping(value = {"/create"})
+  public String createFox(@ModelAttribute("fox") Fox fox){
+    foxes.add(fox);
+    return "redirect:/?name=" + fox.getName();
   }
 }
